2ee5540fe54d5e5dc793fb0bbee7447180714150
Timothy Nordloh <tnordloh@gmail.com>
2015-03-28 16:33:20 -0600

    extract method for calculating x starting point

--- lib/seven_segments.rb
+++ lib/seven_segments.rb
@@ -53,24 +53,36 @@ class SevenSegments
 
   def find_segment_start(this_segment)
     ## James says this probably should be in an array
+    temp_x = x_axis(this_segment)
     case this_segment
     when 0
-     StartingPoint.new(0,1,:across) #segment a (top)
+     StartingPoint.new(temp_x,1,:across) #segment a (top)
     when 1
-     StartingPoint.new(1, @size+1 ,:down) #segment b (TR)
+     StartingPoint.new(temp_x,@size+1 ,:down) #segment b (TR)
     when 2
-     StartingPoint.new( @size+2 , @size+1 ,:down) #c (BR)
+     StartingPoint.new(temp_x,@size+1 ,:down) #c (BR)
     when 3
-     StartingPoint.new( @size*2+2,1,:across) #d (B)
+     StartingPoint.new(temp_x,1,:across) #d (B)
     when 4
-     StartingPoint.new(@size+2 ,0,:down) #e (BL)
+     StartingPoint.new(temp_x,0,:down) #e (BL)
     when 5
-     StartingPoint.new(1,0,:down) #e (TL)
+     StartingPoint.new(temp_x,0,:down) #e (TL)
     when 6
-     StartingPoint.new(@size+1,1,:across) #e (M)
+     StartingPoint.new(temp_x,1,:across) #e (M)
     else
       fail "segment number #{this_segment} out of range"
     end
+  end
+
+  def x_axis(segment)
+    points = {0 => 0,
+             1 => 1,
+             2 => @size + 2,
+             3 => @size * 2 + 2,
+             4 => @size +2,
+             5 => 1 ,
+             6 => @size + 1 }
+    points[segment]
   end
 
   def light_segments
