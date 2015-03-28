75a3d4bfc40d27804c11ccbaada4b5184fc9c127
Timothy Nordloh <tnordloh@gmail.com>
2015-03-28 16:09:35 -0600

    change collect to map

--- lib/lcd.rb
+++ lib/lcd.rb
@@ -5,7 +5,7 @@ class LCD
   require_relative 'seven_segments'
 
   def initialize(digits,size=2)
-    @digits = digits.collect {|digit| SevenSegments.new(digit,size) }
+    @digits = digits.map {|digit| SevenSegments.new(digit,size) }
     @raw_digits=digits
     raise ArgumentError, "#{digits} has a non-digit value" unless valid_values?
   end
