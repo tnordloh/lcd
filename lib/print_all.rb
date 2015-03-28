2e0e443ff6d74fc616a3ebf529072731cc3283a9
Timothy Nordloh <tnordloh@gmail.com>
2015-03-28 14:53:13 -0600

    fix formatting

--- lib/print_all.rb
+++ lib/print_all.rb
@@ -8,11 +8,13 @@ class PrintAll
     @digits = digits.collect {|digit| SevenSegments.new(digit,size) }
     print_all
   end
+
   def print_all
     @digits[0].lcd_size.times {|row|
       @digits.each {|segment| segment.print_row(row) }
       puts
     }
   end
+
 end
 
