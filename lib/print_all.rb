92b44f9f573d253aa12c5f4cd7fd103308cd4e59
Timothy Nordloh <tnordloh@gmail.com>
2015-03-28 16:02:21 -0600

    push print up into initialize call.

--- lib/print_all.rb
+++ lib/print_all.rb
@@ -8,14 +8,15 @@ class PrintAll
     @digits = digits.collect {|digit| SevenSegments.new(digit,size) }
     @raw_digits=digits
     raise ArgumentError, "#{digits} has a non-digit value" unless valid_values?
-    print_all
+    print print_all
   end
 
   def print_all
+    printout = ""
     @digits[0].lcd_size.times {|row|
-      print @digits.map {|segment| segment.to_s(row) }.join + "\n"
-      puts
+      printout << @digits.map {|segment| segment.to_s(row) }.join + "\n"
     }
+    printout
   end
 
   def valid_values?
