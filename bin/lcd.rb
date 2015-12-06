c5dc868b5177934a3cb4afe1ad24b826f54b5202
Timothy Nordloh <tnordloh@gmail.com>
2015-12-05 20:51:12 -0700

    fix argument parsing, just a little

--- bin/lcd.rb
+++ bin/lcd.rb
@@ -2,12 +2,13 @@
 
 require_relative '../lib/lcd'
 
-size=2 
+default_size = 2 
 
 if ARGV.include?("-s")  
   index=ARGV.index "-s"
   if ARGV[index+1]   
-    size=ARGV[index+1].to_i
+    default_size=ARGV[index+1].to_i
+    fail "lcd size is 0 or less; invalid" if (default_size<=0)
   else
     fail "-s provided, but no value provided after it" 
   end
@@ -15,12 +16,11 @@ end
 
 fail "no argument provided" if ARGV[-1].nil?  
 
-fail "lcd size is 0 or less; invalid" if (size<=0)
 
 fail "not a digit" if !!ARGV[-1][/^0-9/]
 digits= ARGV[-1].chars
 
 
-puts LCD.new(digits,size).to_s
+puts LCD.new(digits,size = default_size).to_s
 
 #lcd= Seven_segments.new(ARGV[-1],size)
